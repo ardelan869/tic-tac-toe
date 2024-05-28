bbee3e1477896791533fca1add36d3a76d05ab88
Ardelan <ardelanyamanel@outlook.de>
2024-05-28 20:10:21 +0200

    fix: Removed `@nuxt/ui` module from config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: {
     preference: 'dark',
     fallback: 'dark',
